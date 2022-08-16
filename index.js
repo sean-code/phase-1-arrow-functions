2f49d5b0ef14c1af171f2a6a825f8f2ed1361059
sean-code <john.nganga@student.moringaschool.com>
2022-08-16 20:05:29 +0300

    Completed Lab

---
+++ index.js
@@ -0,0 +1,8 @@
+let divide = (number1, number2)=> (2000 / 100);
+divide();
+
+let square = sameNumber => sameNumber * sameNumber;
+square();
+
+let add = (value1, value2) => value1 + value2;
+add();
